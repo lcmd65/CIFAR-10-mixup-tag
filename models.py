bc2c5c6e5660e1b321aba0183b17a23eff17a195
Dat <dat.lemindast@gmail.com>
2024-02-17 13:08:07 +0700

    change

---
+++ models.py
@@ -0,0 +1,14 @@
+from torch import SimpleDLA
+
+class SimpleDLA:
+    def __init__(self):
+        self.model = SimpleDLA()
+        
+    def train():
+        pass
+    
+    def test():
+        pass
+        
+    def validate():
+        pass
